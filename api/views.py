b44bfe52b1861e1091727a4177f58c769154a18d
enuii3 <b.nxxxi551@gmail.com>
2022-03-23 17:40:12 +0900

    feat: LanguageSerializerの作成

--- api/views.py
+++ api/views.py
@@ -1,6 +1,6 @@
 from rest_framework import generics, permissions, viewsets
-from .serializers import UserSerializer, ProfileSerializer
-from .models import User, Profile
+from .serializers import UserSerializer, ProfileSerializer, LanguageSerializer
+from .models import User, Profile, Language
 from .permissions import IsOwnerOrReadOnly
 
 
@@ -29,3 +29,8 @@ class ProfileViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+
+
+class RetrieveLanguage(generics.RetrieveAPIView):
+    queryset = Language.objects.all()
+    serializer_class = LanguageSerializer
